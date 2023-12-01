7c4b5204ec6b29f8960412be312db27da1223eba
ehill-iolani <ehill@iolani.org>
2023-12-01 11:43:32 -1000

    bug fixes + progress messages

--- app.R
+++ app.R
@@ -1,18 +1,16 @@
 library(shiny)
-#library(shinydashboard)
 library(ggplot2)
 library(dplyr)
 library(stringr)
-#library(tidyr)
 library(plotly)
 
 # Define UI for application, create sidebar and body
 ui <- shinyUI(fluidPage(
-  titlePanel("AIN EMU 16S Classifier"),
+  titlePanel("AIN EMU 16S Classifier (beta)"),
   sidebarLayout(
     sidebarPanel(
       helpText("Select fastq or fastq.gz files you want to classify"),
-      fileInput("fastqs", "Upload .fastq or fastq.gz files", accept = c(".fastq", ".fastq.gz"), multiple = TRUE),
+      fileInput("fastqs", "Upload .fastq or fastq.gz files", accept = c(".fastq", ".gz"), multiple = TRUE),
       numericInput("lowerlength", "Minimum amplicon length", value = 1300),
       numericInput("upperlength", "Maximum amplicon length", value = 1700),
       numericInput("quality", "Minimum Quality Score", value = 10),
@@ -26,8 +24,8 @@ ui <- shinyUI(fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   # When the run button is clicked, execute the workflow
-  eventReactive(input$run, {
-  showModal(modalDialog("Running the EMU pipeline, please be patient...", footer = NULL))
+  observeEvent(input$run, {
+  showModal(modalDialog("Running the EMU pipeline, please be patient...ingesting files", footer = NULL))
 
     # Make a directory to store input files
     dir.create(file.path("/home/staging"))
@@ -55,20 +53,24 @@ server <- function(input, output) {
     # Check if the files are fastq or fastq.gz; unzip if necessary
     if(any(str_detect(files, ".gz"))) {
       system("gunzip barcode*/*.gz")
+      system("for i in */; do cd $i; for j in *; do mv $j ${j}.fastq; done; cd ..; done")
     }
+
+    removeModal()
+    showModal(modalDialog("Running the EMU pipeline, please be patient...filtering files", footer = NULL))
 
     # Create processing directory and set working directory
     dir.create(file.path("/home", "processing"))
 
     # Cat all the fastqs in a directory into one file and
     # write to processing directory
-    system("for i in barcode*; do cat $i/*.fastq >> ../processing/${i}_unfiltered.fastq; done")
+    system("for i in */; do j=${i%/}; cat $j/*.fastq >> /home/processing/${j}_unfiltered.fastq; done")
 
     # Set working directory to processing
     setwd("/home/processing")
 
     # Filter reads by length and quality using nanofilt
-    system(paste("for i in barcode*; do conda run -n emu NanoFilt -l", input$lowerlength, "--maxlength", input$upperlength, "-q", input$quality, "-t", input$threads, "-o", "${i}_filtered.fastq", "$i.fastq; done"))
+    system(paste("for i in *.fastq; do j=${i%_*}; conda run -n emu NanoFilt -l", input$lowerlength, "--maxlength", input$upperlength, "-q", input$quality, "$i > ${j}_filtered.fastq; done"))
 
     # Remove the unfiltered fastq files
     system("rm *_unfiltered.fastq")
@@ -76,8 +78,11 @@ server <- function(input, output) {
     # Create a directory to store results
     dir.create(file.path("/home", "results"))
 
+    removeModal()
+    showModal(modalDialog("Running the EMU pipeline, please be patient...running EMU", footer = NULL))
+
     # Run EMU
-    system("for i in *.fastq; conda run -n emu emu abundance --keep-counts --output-unclassified --output-dir ../results --threads 24 $i; done")
+    system(paste("for i in *.fastq; do conda run -n emu emu abundance --keep-counts --output-unclassified --output-dir ../results --threads", input$threads, "$i; done"))
 
     # Set working directory to results
     setwd("/home/results")
