41bda7de705ea1c97d7fabead73e72dcdaee37e9
ehill-iolani <ehill@iolani.org>
2023-12-04 15:18:39 -1000

    added standalone results viewer

--- app.R
+++ app.R
@@ -3,29 +3,76 @@ library(ggplot2)
 library(dplyr)
 library(stringr)
 library(plotly)
+library(shinydashboard)
+library(shinyalert)
 
-# Define UI for application, create sidebar and body
-ui <- shinyUI(fluidPage(
-  titlePanel("AIN EMU 16S Classifier (beta)"),
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Select fastq or fastq.gz files you want to classify"),
-      fileInput("fastqs", "Upload .fastq or fastq.gz files", accept = c(".fastq", ".gz"), multiple = TRUE),
-      numericInput("lowerlength", "Minimum amplicon length", value = 1300),
-      numericInput("upperlength", "Maximum amplicon length", value = 1700),
-      numericInput("quality", "Minimum Quality Score", value = 10),
-      numericInput("threads", "Threads", value = 2),
-      actionButton("run", "Run EMU!")),
-    mainPanel(
-      plotlyOutput("results"))
-  )
-))
+ui <- dashboardPage(
+  dashboardHeader(title = "AIN EMU 16S Classifier (beta)"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Emu Classifier & Visualizer", tabName = "emu_class", icon = icon("upload")),
+    menuItem("Emu Visualizer", tabName = "emu_viz", icon = icon("upload")),
+    menuItem("Relative Abundance", tabName = "relab_plot", icon = icon("chart-simple")),
+    menuItem("Bray Curtis PCoA", tabName = "pcoa", icon = icon("chart-simple")),
+    menuItem("Rarefaction Curve", tabName = "rare", icon = icon("chart-simple"))
+  )),
+  dashboardBody(tabItems(
+    tabItem(tabName = "emu_class",
+            fluidRow(
+              box(
+                title = "File Upload", status = "primary", solidHeader = TRUE, width = 12,
+                fileInput("fastqs", "Upload .fastq or fastq.gz files", accept = c(".fastq", ".gz"), multiple = TRUE),
+                  numericInput("lowerlength", "Minimum amplicon length", value = 1300),
+                  numericInput("upperlength", "Maximum amplicon length", value = 1700),
+                  numericInput("quality", "Minimum Quality Score", value = 10),
+                  numericInput("threads", "Threads", value = 2),
+                  actionButton("run_class", "Run EMU Classifier + Visualizer!")))),
+    tabItem(tabName = "emu_viz",
+            fluidRow(
+              box(
+                title = "File Upload", status = "primary", solidHeader = TRUE, width = 12,
+                fileInput("tsv", "Upload EMU .tsv classifications", accept = c(".txt", ".tsv"), multiple = TRUE),
+                actionButton("run_viz", "Run EMU Visualizer!")))),
+    tabItem(tabName = "relab_plot",
+            fluidRow(
+              box(
+                title = "Relative Abundance",
+                status = "primary",
+                solidHeader = TRUE,
+                width = 12,
+                plotlyOutput("relabresults")))),
+    tabItem(tabName = "pcoa",
+            fluidRow(
+              box(
+                title = "Bray Curtis PCoA",
+                status = "primary",
+                solidHeader = TRUE,
+                width = 12,
+                plotlyOutput("pcoaresults")))),
+    tabItem(tabName = "rare",
+            fluidRow(
+              box(
+                title = "Rarefaction Curve",
+                status = "primary",
+                solidHeader = TRUE,
+                width = 12,
+                plotlyOutput("rareresults"))))
+  ))
+)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-  # When the run button is clicked, execute the workflow
-  observeEvent(input$run, {
-  showModal(modalDialog("Running the EMU pipeline, please be patient...ingesting files", footer = NULL))
+server <- shinyServer(function(input, output) {
+  # Runs the classifier and visualizer
+  observeEvent(input$run_class, {
+    if (is.null(input$fastqs)) {
+      shinyalert(
+        title = "Error!",
+        text = "Please upload fastq files.",
+        type = "error",
+        showConfirmButton = TRUE,
+        confirmButtonText = "OK",
+      )
+      return(NULL)
+    } else {
+    showModal(modalDialog("Running the workflow, please be patient...ingesting files", footer = NULL))
 
     # Make a directory to store input files
     dir.create(file.path("/home/staging"))
@@ -55,7 +102,7 @@ server <- function(input, output) {
     }
 
     removeModal()
-    showModal(modalDialog("Running the EMU pipeline, please be patient...filtering files", footer = NULL))
+    showModal(modalDialog("Running the workflow, please be patient...filtering reads using NanoFilt", footer = NULL))
 
     # Create processing directory and set working directory
     # Cat all the fastqs in a directory into one file and
@@ -72,13 +119,13 @@ server <- function(input, output) {
     dir.create(file.path("/home", "results"))
 
     removeModal()
-    showModal(modalDialog("Running the EMU pipeline, please be patient...running EMU", footer = NULL))
+    showModal(modalDialog("Running the workflow, please be patient...classifying reads using EMU", footer = NULL))
 
     # Run EMU
     system(paste("for i in *.fastq; do conda run -n emu emu abundance --db /home/database --keep-counts --output-unclassified --output-dir ../results --threads", input$threads, "$i; done"))
 
     removeModal()
-    showModal(modalDialog("Running the EMU pipeline, please be patient...visualizing the results", footer = NULL))
+    showModal(modalDialog("Running the workflow, please be patient...visualizing the using ggplotly", footer = NULL))
 
     # Read in the EMU results
     setwd("/home/results")
@@ -93,7 +140,7 @@ server <- function(input, output) {
     }
 
     # Plot the relative abundance of each taxon by barcode
-    output$results <- renderPlotly({
+    output$relabresults <- renderPlotly({
       ggplotly(ggplot(emu_out_all, aes(x = barcode, y = abundance, fill = genus)) +
             geom_bar(stat = "identity", position = "fill") +
             theme_classic() +
@@ -103,8 +150,62 @@ server <- function(input, output) {
     })
 
     removeModal()
-  })
-}
+    shinyalert(
+      title = "Completed!",
+      text = "EMU Classifier + Visualizer has finished running! Results can be viewed under the Relative Abundance, Bray Curtis PCoA, and Rarefaction Curve tabs.",
+      type = "success",
+      showConfirmButton = TRUE,
+      confirmButtonText = "OK",
+    )
+  }})
 
-# Run the application
+  # Runs only the visualizer
+  observeEvent(input$run_viz, {
+    if (is.null(input$tsv)) {
+      shinyalert(
+        title = "Error!",
+        text = "Please upload EMU .tsv files.",
+        type = "error",
+        showConfirmButton = TRUE,
+        confirmButtonText = "OK",
+      )
+      return(NULL)
+    } else {
+    showModal(modalDialog("Running the workflow, please be patient...visualizing the using ggplotly", footer = NULL))
+
+    # Read in the EMU classifications and combine into 1 dataframe and parsing out the barcode from filename
+    files <- input$tsv
+    if (is.list(files)) {
+      emu_out_all <- data.frame()
+      for (i in seq_along(files$name)) {
+        temp <- read.delim(files$datapath[i], header = TRUE, sep = "\t") %>%
+                mutate(barcode = str_extract(files$name[i], "barcode[0-9]+"))
+        emu_out_all <- rbind(emu_out_all, temp)
+      }
+    } else {
+      emu_out_all <- read.delim(files$datapath, header = TRUE, sep = "\t") %>%
+                      mutate(barcode = str_extract(files$name, "barcode[0-9]+"))
+    }
+
+    # Plot the relative abundance of each taxon by barcode
+    output$relabresults <- renderPlotly({
+      ggplotly(ggplot(emu_out_all, aes(x = barcode, y = abundance, fill = genus)) +
+            geom_bar(stat = "identity", position = "fill") +
+            theme_classic() +
+            theme(axis.text.x = element_text(angle = 0, hjust = .5)) +
+            labs(x = "Barcode", y = "Relative abundance (%)", fill = "Genus") +
+            ggtitle("Relative abundance of genera per sample"))
+    })
+
+    removeModal()
+    shinyalert(
+      title = "Completed!",
+      text = "EMU Visualizer has finished running! Results can be viewed under the Relative Abundance, Bray Curtis PCoA, and Rarefaction Curve tabs.",
+      type = "success",
+      showConfirmButton = TRUE,
+      confirmButtonText = "OK",
+    )
+  }})
+})
+
 shinyApp(ui = ui, server = server)
